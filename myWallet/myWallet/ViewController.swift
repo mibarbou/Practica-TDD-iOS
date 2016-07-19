8b47f8cc01e0b40f6d6c502c06597a9b8f9877eb
Michel Barbou Salvador <mibarbou@gmail.com>
2016-07-19 13:18:27 +0200

    Subida Inicial del proyecto xCode y el .gitignore

---
+++ myWallet/myWallet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myWallet
+//
+//  Created by Michel Barbou Salvador on 19/07/16.
+//  Copyright © 2016 mibarbou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
